b667ad61d5206cfc66ac3fda235cc5f68cfb21f9
Deep DeskROG <dghaghara@gmail.com>
2019-01-30 23:36:23 -0500

    DiceeApp

---
+++ DiceeApp/app/src/main/java/com/example/deep1/dicee/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.deep1.dicee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button rollbutton = findViewById(R.id.rollButton);
+
+        final ImageView leftDice = findViewById(R.id.image_leftDice);
+        final ImageView rightDice = findViewById(R.id.image_rightDice);
+
+        final int[] diceArray = {R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6};
+
+        rollbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random rand = new Random();
+
+                int numL = rand.nextInt(6);
+                int numR = rand.nextInt(6);
+                Log.d("Dicee", "Numbers are [" + numL + "," + numR + "]");
+                leftDice.setImageResource(diceArray[numL]);
+
+                rightDice.setImageResource(diceArray[numR]);
+
+            }
+        });
+
+    }
+}
